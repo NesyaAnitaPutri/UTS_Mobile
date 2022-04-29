2168f655d23ad277934b27e2ba6d5f1bf16ea042
NesyaAnitaPutri <lampungbandar408@gmail.com>
2022-04-29 16:52:52 +0700

    first commit

---
+++ app/src/main/java/com/example/projek/Info.java
@@ -0,0 +1,32 @@
+package com.example.projek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class Info extends AppCompatActivity {
+    Button btkembali;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info);
+
+        btkembali = (Button) findViewById(R.id.btkembali);
+
+        btkembali.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Info.this, MenuUtama.class);
+                finish();
+                startActivity(intent);
+            }
+        });
+    }
+}
